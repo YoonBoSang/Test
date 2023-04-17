0750b5170c9d2a56acbd6135c9a0e67f098c7012
BoSang Yoon <qhtkddl002@naver.com>
2023-04-17 17:50:44 +0900

    23-04-17

--- Test102.java
+++ Test102.java
@@ -1,5 +1,24 @@
 package Test;
 
 public class Test102 {
-
+	public static void main(String[] args) {
+		int number = 5;
+		int limit = 3;
+		int power = 2;
+        int answer = 0;
+        int sum = 1;
+        for(int i = 1; i <= number; i++) {
+            for(int j = 1; j <= i/2; j++) {
+                if(i % j == 0) {
+                    sum++;
+                }
+            }
+            if(sum <= limit) {
+                answer += sum;
+            } else {
+                answer += power;
+            }
+            sum = 1;
+        }
+	}
 }
