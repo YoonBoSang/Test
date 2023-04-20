a2a2ffc1f802500664cc91bb357c814a4cc32ab0
BoSang Yoon <qhtkddl002@naver.com>
2023-04-20 17:50:17 +0900

    23-04-20

---
+++ Test104.java
@@ -0,0 +1,22 @@
+package Test;
+
+import java.util.*;
+
+public class Test104 {
+	public static void main(String[] args) {
+		String s = "baabaa";
+		System.out.println(" ".toLowerCase());
+        String[] arr = s.split("");
+        Stack<String> stack = new Stack<>();
+        for(int i = 0; i < arr.length; i++) {
+            if(stack.size() != 0 && arr[i].equals(stack.peek())) {
+                stack.pop();
+                continue;
+            }
+            stack.push(arr[i]);
+        }
+        if(stack.size() == 0) {
+//            return 1;
+        }
+	}
+}
